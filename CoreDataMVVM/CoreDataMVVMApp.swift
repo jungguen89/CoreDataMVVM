71b259fb9fef4428ad18053c3b7e1270612ab209
jungguen89 <leral3@nate.com>
2023-03-25 04:55:27 +0900

    Initial Commit

---
+++ CoreDataMVVM/CoreDataMVVMApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoreDataMVVMApp.swift
+//  CoreDataMVVM
+//
+//  Created by 권정근 on 2023/03/25.
+//
+
+import SwiftUI
+
+@main
+struct CoreDataMVVMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
